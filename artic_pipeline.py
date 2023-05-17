756fa21e135598dc0a882e7538aed8e46b35139c
DuyenLe2000 <128071771+DuyenLe2000@users.noreply.github.com>
2023-05-17 14:59:26 +0200

    Update artic_pipeline.py

--- artic_pipeline.py
+++ artic_pipeline.py
@@ -10,18 +10,17 @@ with open("variables_for_artic.txt", "r") as fin:
 
 path_to_reads = variable_list[0]
 barcode = path_to_reads.split("/")[-2]
-run_name = variable_list[1]
-medaka_model = variable_list[2]
-path_to_primer_scheme = variable_list[3]
-version = variable_list[4]
-sample = variable_list[5]
-output_dir = variable_list[6]
+medaka_model = variable_list[1]
+path_to_primer_scheme = variable_list[2]
+version = variable_list[3]
+sample = variable_list[4]
+output_dir = variable_list[5]
 
 os.system(f"mv variables_for_artic.txt {output_dir}")
 os.chdir(output_dir)
 
 # Read filtering
-os.system(f"artic guppyplex --skip-quality-check --min-length 350 --max-length 900 --directory {path_to_reads[:-1]} --prefix {run_name}")
+os.system(f"artic guppyplex --skip-quality-check --min-length 350 --max-length 900 --directory {path_to_reads[:-1]} --prefix demultiplexed")
 
 # Medaka
 os.system(f"artic minion --medaka --medaka-model {medaka_model} --normalise 200 --threads 4 --scheme-directory {path_to_primer_scheme[:-1]} --read-file {run_name}_{barcode}.fastq RSV-2023/{version} {sample}")
