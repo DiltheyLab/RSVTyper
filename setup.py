79d7f7a6e42ade9b7a999006c969c75b9156dae6
DuyenLe2000 <duyen-le@outlook.com>
2023-07-04 19:24:53 +0200

    added package stuff

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ setup(name="rsv-typer",
       version="0.1",
       description="Genotyping RSV samples from nanopore sequencing data",
       url="https://github.com/DiltheyLab/RSVTyper",
+      packages=["rsv-typer"],
+      package_dir={"rsv-typer": "rsv-typer"},
+      package_data={"rsv-typer": ["references/*", "primer_scheme/*"],
       entry_points={
           "console_scripts": [
-              "rsvtyper=process_RSV_sample:main"
+              "rsv-typer=process_RSV_sample:main"
               ]
           }
       )
+
 
