44b5ca9e19128d77851f753c99794fb71e4830de
DuyenLe2000 <duyen-le@outlook.com>
2023-07-04 18:30:09 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name="rsv-typer",
+      version="0.1",
+      description="Genotyping RSV samples from nanopore sequencing data",
+      url="https://github.com/DiltheyLab/RSVTyper",
+      entry_points={
+          "console_scripts": [
+              "rsvtyper=process_RSV_sample:main"
+              ]
+          }
+      )
+
