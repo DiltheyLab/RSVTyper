47736980c116687ddce48b187a86d1d72dc42a4c
DuyenLe2000 <duyen-le@outlook.com>
2023-07-10 16:40:36 +0200

    testing find_packages

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(name="rsv-typer",
       version="0.1",
       description="Genotyping RSV samples from nanopore sequencing data",
       url="https://github.com/DiltheyLab/RSVTyper",
-      packages=["rsv-typer"],
-      package_dir={"rsv-typer": "rsv_typer"},
-      package_data={"rsv-typer": ["references/*", "primer_scheme/*"]},
+      packages = find_packages()
+      #packages=["rsv_typer"],
+      #package_dir={"rsv-typer": "rsv_typer"},
+      #package_data={"rsv-typer": ["references/*", "primer_scheme/*"]},
       entry_points={
           "console_scripts": [
               "rsv-typer=rsv_typer.process_RSV_sample:main"
